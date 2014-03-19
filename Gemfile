aec08a330e9ef2394b5c4aee836b433d6f12d6c5
Jackson Zhang <zbcjackson@gmail.com>
2014-03-19 12:17:21 +0800

    Create successfully; Deny duplicated name

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ gem 'haml'      # Haml模版
 gem 'sinatra'   # Sinatra
 gem 'thin'      # 应用服务器
 gem 'shotgun'   # 重启服务器
+gem 'rerun'
 group :test do
   gem 'rspec'     # 单元测试
 end
--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require 'sinatra'
+require './person'
+
+set :persons, []
+
 get '/' do
   erb :index
+end
+
+post '/' do
+  @persons = settings.persons
+  person = Person.new
+  person.name = request["name"]
+  person.title = request["title"]
+
+  if person.name.empty?
+    redirect to('/?result=Name is required')
+  end
+
+  @persons.push(person)
+  redirect to('/list')
+end
+
+get '/list' do
+  @persons = settings.persons
+  erb :list
 end
